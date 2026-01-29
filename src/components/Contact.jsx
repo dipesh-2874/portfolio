b50de17794cb64c96a3019df6b0d2be58b314314
Cerebro-184 <mr.dipesh184@gmail.com>
2026-01-29 06:16:29 +0530

    repo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import emailjs from '@emailjs/browser'
+import "./Contact/contact.css"
 
 const Contact = ({ id }) => {
   const form = useRef()
@@ -31,17 +32,17 @@ const Contact = ({ id }) => {
         </p>
 
         <form ref={form} onSubmit={sendEmail} className='text-white w-1/2 flex flex-col gap-4 mt-7'>
-          <input name='name' className='border-2 border-emerald-300 h-10 px-4 py-2 rounded-xl' type="text" placeholder='Name' required/>
-          <input name='email' className='border-2 border-emerald-300 h-10 px-4 py-2 rounded-xl' type="email" placeholder='Email' required/>
-          <input name='subject' className='border-2 border-emerald-300 h-10 px-4 py-2 rounded-xl' type="text" placeholder='Subject' required/>
-          <textarea name="message" className='border-2 border-emerald-300 resize-none h-40 px-4 py-4 rounded-xl' placeholder='Message' required></textarea>
+          <input name='name' className='border-2 outline-0 border-emerald-300 h-10 px-4 py-2 rounded-xl' type="text" placeholder='Name' required/>
+          <input name='email' className='border-2 outline-0 border-emerald-300 h-10 px-4 py-2 rounded-xl' type="email" placeholder='Email' required/>
+          <input name='subject' className='border-2 outline-0 border-emerald-300 h-10 px-4 py-2 rounded-xl' type="text" placeholder='Subject' required/>
+          <textarea name="message" className='border-2 outline-0 border-emerald-300 resize-none h-40 px-4 py-4 rounded-xl' placeholder='Message' required></textarea>
 
           <div className="btns px-5 mt-3 flex flex-row justify-between">
-            <button type="reset" className="flex py-2 px-3 items-center gap-2 bg-emerald-300 text-zinc-950 font-extrabold rounded-xl">
-              <box-icon name='reset' color='#09090b'></box-icon>
+            <button type="reset" className="cb flex py-2 px-3 items-center gap-2 border-2 border-emerald-300 text-emerald-300 font-extrabold rounded-xl">
+              <box-icon name='reset' color='#03fac0'></box-icon>
               Reset
             </button>
-            <button type="submit" className="flex py-2 px-3 items-center gap-2 bg-emerald-300 text-zinc-950 font-extrabold rounded-xl">
+            <button type="submit" className="cb flex py-2 px-3 items-center gap-2 bg-emerald-300 text-zinc-950 font-extrabold rounded-xl">
               <box-icon name='send' type='solid' color='#09090b'></box-icon>
               Send
             </button>
